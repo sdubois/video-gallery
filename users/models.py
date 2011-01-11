1e38f1d41e16608b975b98ae57b33a1b73dab6cc
Steven DuBois <sdubois@csh.rit.edu>
2011-01-11 00:18:17 -0500

    removed old files

--- users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class UID (models.Model):
-    name = models.TextField(unique = True)
-    email = models.EmailField(unique = True)
-    password = models.TextField()
-
