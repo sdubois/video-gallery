0f62ef3dbecdde069b7524c2a95a10a51214f8f7
Steven DuBois <sdubois@csh.rit.edu>
2011-01-11 13:34:44 -0500

    ko

--- keller/settings.py
+++ keller/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'videogallery',                      # Or path to database file if using sqlite3.
         'USER': 'videogallery',                      # Not used with sqlite3.
-        'PASSWORD': 'komeansinfinty',                  # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'db.csh.rit.edu',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
