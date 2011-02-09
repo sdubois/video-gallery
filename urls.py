db02b894792033565ac3d1cd54874555422f2aca
Steven DuBois <sdubois@csh.rit.edu>
2011-02-08 21:00:42 -0500

    fixed template bug

--- urls.py
+++ urls.py
@@ -20,5 +20,7 @@ urlpatterns = patterns('',
 (r'^$', main_page),
 (r'^user/(\w+)/$', user_page),
 (r'^login/$', 'django.contrib.auth.views.login'),
+(r'^logout/$', logout_page),
+#(r'^admin/', include(admin.site.urls)),
 )
                        
--- videogallery/views.py
+++ videogallery/views.py
@@ -1,19 +1,16 @@
 # Create your views here.
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import Context
 from django.template.loader import get_template
 from django.contrib.auth.models import User
+from django.contrib.auth import logout, login
+from django.shortcuts import render_to_response
 
 def main_page(request):
-    template = get_template('main_page.html')
-    variables = Context({
-            'head_title': u'Video Gallery',
-            'page_title': u'Welcome to Video Gallery',
-            'page_body': u'Where you can upload videos',
-            'user': request.user
-    })
-    output = template.render(variables)
-    return HttpResponse(output)
+    return render_to_response(
+        'main_page.html',
+        {'user': request.user}
+)
 
 def user_page(request, username):
     try:
@@ -28,3 +25,7 @@ def user_page(request, username):
     })
     output = template.render(variables)
     return HttpResponse(output)
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
