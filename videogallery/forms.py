17e1137ed0781d561038d8e79923cb147ff29c5d
Steven DuBois <sdubois@csh.rit.edu>
2011-02-13 02:51:16 -0500

    added forms

--- videogallery/forms.py
+++ videogallery/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+import re
+from django.contrib.auth.models import User
 
 class RegistrationForm(forms.Form):
     username = forms.CharField(label=u'Username', max_length=30)
@@ -19,3 +21,15 @@ def clean_password2(self):
         if password1 == password2:
             return password2
     raise forms.ValidationError('Passwords do not match.')
+
+def clean_username(self):
+    username = self.cleaned_data['username']
+    if not re.search(r'^\w+$', username):
+        raise forms.ValidationError('Username can only contain '
+                                    'alphanumeric characters and'
+                                    'the underscore.')
+    try:
+        User.objects.get(username=username)
+    except User.DoesNotExist:
+        return username
+    raise forms.ValidationError('Username is
